ee11a327806136b5ee0f7b82ac7eee8d5b8b5c23
Mas <mas@Mass-MacBook-Pro.local>
2019-04-21 23:36:10 -0400

    first commit

--- config/database.js
+++
@@ -1,5 +0,0 @@
-// config/database.js
-module.exports = {
-    'url' : 'mongodb+srv://samcasseusdev:interviewing@interviewing-ojseq.mongodb.net/interviewing?retryWrites=true', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'interviewing'
-};
